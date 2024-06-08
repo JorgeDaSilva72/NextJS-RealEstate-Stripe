a8e8a6baa4724cb105605095e1f62ebece05da19
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-06-08 14:18:17 +0200

    Creating SignInPanel component.

---
+++ src/app/components/signInPanel.tsx
@@ -0,0 +1,38 @@
+import {
+  LoginLink,
+  LogoutLink,
+  RegisterLink,
+  getKindeServerSession,
+} from "@kinde-oss/kinde-auth-nextjs/server";
+import { Button } from "@nextui-org/react";
+import React from "react";
+// import UserProfilePanel from "./UserProfilePanel";
+import prisma from "@/lib/prisma";
+
+const signInPanel = async () => {
+  const { isAuthenticated, getUser } = await getKindeServerSession();
+  if (await isAuthenticated()) {
+    const user = await getUser();
+    const dbUser = await prisma.user.findUnique({
+      where: {
+        id: user?.id,
+      },
+    });
+    return <>{user?.given_name} </>;
+
+    // return <>{dbUser!! && <UserProfilePanel user={dbUser} />}</>;
+  }
+
+  return (
+    <div className="flex gap-3">
+      <Button color="primary">
+        <LoginLink>Se connecter</LoginLink>
+      </Button>
+      <Button>
+        <RegisterLink>Créer un compte</RegisterLink>
+      </Button>
+    </div>
+  );
+};
+
+export default signInPanel;
