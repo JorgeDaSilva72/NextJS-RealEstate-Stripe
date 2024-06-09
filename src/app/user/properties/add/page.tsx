adc5df34c2e75aaf22a5534200f6bd46e05d6a77
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-06-09 11:15:47 +0200

    User Properties Page Introduction.

---
+++ src/app/user/properties/add/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>add page</div>;
+};
+
+export default page;
---
+++ src/app/user/properties/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Add page</div>;
+};
+
+export default page;
