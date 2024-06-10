ba332cdd983552935251e835c4b4743c29caed1b
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-06-10 14:40:31 +0200

    Creating User Property Page with pagination.

--- src/app/user/properties/page.tsx
+++ src/app/user/properties/page.tsx
@@ -1,7 +1,53 @@
+import prisma from "@/lib/prisma";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import React from "react";
+import PropertiesTable from "./_components/PropertiesTable";
 
-const page = () => {
-  return <div>Add page</div>;
+const PAGE_SIZE = 2;
+
+interface Props {
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
+const PropertiesPage = async ({ searchParams }: Props) => {
+  const { getUser } = await getKindeServerSession();
+  const user = await getUser();
+
+  const pagenum = searchParams.pagenum ?? 1;
+  const propertiesPromise = prisma.property.findMany({
+    where: {
+      userId: user?.id,
+    },
+    include: {
+      type: true,
+      status: true,
+    },
+    skip: (+pagenum - 1) * PAGE_SIZE,
+    take: PAGE_SIZE,
+  });
+
+  const totalPropertiesPromise = prisma.property.count({
+    where: {
+      userId: user?.id,
+    },
+  });
+
+  const [properties, totalProperties] = await Promise.all([
+    propertiesPromise,
+    totalPropertiesPromise,
+  ]);
+
+  const totalPages = Math.floor(totalProperties / PAGE_SIZE);
+
+  console.log({ properties });
+
+  return (
+    <PropertiesTable
+      properties={properties}
+      totalPages={totalPages}
+      currentPage={+pagenum}
+    />
+  );
 };
 
-export default page;
+export default PropertiesPage;
