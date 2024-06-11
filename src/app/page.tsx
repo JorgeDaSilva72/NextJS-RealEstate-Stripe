cc6664a6d5d067a8ce9680af6412c7f702c6e72a
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-06-11 19:22:08 +0200

    Integrating Search Bar With useDebounce.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 import prisma from "@/lib/prisma";
-import Image from "next/image";
+
 import PropertyCard from "./components/PropertyCard";
 import PropertyContainer from "./components/PropertyContainer";
-
+import Search from "./components/Search";
 const PAGE_SIZE = 8;
 
 interface Props {
   searchParams: { [key: string]: string | string[] | undefined };
 }
 export default async function Home({ searchParams }: Props) {
-  const pagenum = searchParams.pagenum ?? 1;
-
+  const pagenum = searchParams.pagenum ?? 0;
+  const query = searchParams.query ?? "";
   const propertiesPromise = prisma.property.findMany({
     select: {
       id: true,
@@ -28,21 +28,36 @@ export default async function Home({ searchParams }: Props) {
         },
       },
     },
-    skip: (+pagenum - 1) * PAGE_SIZE,
+    ...(!!query && {
+      where: {
+        name: {
+          contains: String(query),
+        },
+      },
+    }),
+    skip: +pagenum * PAGE_SIZE,
     take: PAGE_SIZE,
   });
-
-  const totalPropertiesPromise = prisma.property.count();
+  const totalPropertiesPromise = prisma.property.count({
+    ...(!!query && {
+      where: {
+        name: {
+          contains: String(query),
+        },
+      },
+    }),
+  });
 
   const [properties, totalProperties] = await Promise.all([
     propertiesPromise,
     totalPropertiesPromise,
   ]);
 
-  const totalPages = Math.floor(totalProperties / PAGE_SIZE) + 1; // Correction +1 pour le rste
+  const totalPages = Math.floor(totalProperties / PAGE_SIZE);
 
   return (
     <div>
+      <Search />
       <PropertyContainer totalPages={totalPages} currentPage={+pagenum}>
         {properties.map((propertyItem) => (
           <PropertyCard property={propertyItem} key={propertyItem.id} />
