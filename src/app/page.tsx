b331ce5401404ddd8a5c9b2d8b3789365ace45f3
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-06-11 14:11:21 +0200

    Creating the Landing Page with the creation of PropertyCard and PropertyContainer components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,40 @@
+import prisma from "@/lib/prisma";
 import Image from "next/image";
+import PropertyCard from "./components/PropertyCard";
+import PropertyContainer from "./components/PropertyContainer";
 
-export default function Home() {
-  return <div></div>;
+interface Props {
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+export default async function Home({ searchParams }: Props) {
+  const propertiesPromise = prisma.property.findMany({
+    select: {
+      id: true,
+      name: true,
+      price: true,
+      images: {
+        select: {
+          url: true,
+        },
+      },
+      location: {
+        select: {
+          city: true,
+          state: true,
+        },
+      },
+    },
+  });
+
+  const [properties] = await Promise.all([propertiesPromise]);
+
+  return (
+    <div>
+      <PropertyContainer>
+        {properties.map((propertyItem) => (
+          <PropertyCard property={propertyItem} key={propertyItem.id} />
+        ))}
+      </PropertyContainer>
+    </div>
+  );
 }
