61d182611766164d86da06cbef1abc3346a55f17
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-06-11 16:28:15 +0200

    Integration Pagination in the landing page with Server Components.

---
+++ src/app/components/PaginationContainer.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { Pagination } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+
+interface Props {
+  totalPages: number;
+  currentPage: number;
+  route?: string; //to make the component more reuseable
+}
+const PaginationContainer = ({
+  totalPages,
+  currentPage,
+  route = "/",
+}: Props) => {
+  const router = useRouter();
+  if (totalPages <= 1) return null;
+
+  return (
+    <Pagination
+      total={totalPages}
+      initialPage={1}
+      page={currentPage}
+      onChange={(page) => router.push(`${route}?pagenum=${page}`)}
+    />
+  );
+};
+
+export default PaginationContainer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,14 @@ import Image from "next/image";
 import PropertyCard from "./components/PropertyCard";
 import PropertyContainer from "./components/PropertyContainer";
 
+const PAGE_SIZE = 8;
+
 interface Props {
   searchParams: { [key: string]: string | string[] | undefined };
 }
 export default async function Home({ searchParams }: Props) {
+  const pagenum = searchParams.pagenum ?? 1;
+
   const propertiesPromise = prisma.property.findMany({
     select: {
       id: true,
@@ -24,13 +28,22 @@ export default async function Home({ searchParams }: Props) {
         },
       },
     },
+    skip: (+pagenum - 1) * PAGE_SIZE,
+    take: PAGE_SIZE,
   });
 
-  const [properties] = await Promise.all([propertiesPromise]);
+  const totalPropertiesPromise = prisma.property.count();
+
+  const [properties, totalProperties] = await Promise.all([
+    propertiesPromise,
+    totalPropertiesPromise,
+  ]);
+
+  const totalPages = Math.floor(totalProperties / PAGE_SIZE) + 1; // Correction +1 pour le rste
 
   return (
     <div>
-      <PropertyContainer>
+      <PropertyContainer totalPages={totalPages} currentPage={+pagenum}>
         {properties.map((propertyItem) => (
           <PropertyCard property={propertyItem} key={propertyItem.id} />
         ))}
