b758c70247d2c4789ffca611a3ab39c3f2d3e94b
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-07-05 14:30:19 +0200

    Bug in PropertyCard component in case not url image found.

--- src/app/components/PropertyCard.tsx
+++ src/app/components/PropertyCard.tsx
@@ -26,26 +26,35 @@ interface Props {
 const PropertyCard = ({ property }: Props) => {
   return (
     <Card className="w-72 flex flex-col hover:scale-105" shadow="md">
-      <Image
-        radius="none"
-        src={
-          property.images[0].url
-          // property.id === 1
-          //   ? property.images[0].url
-          //   : `/images/${Math.floor(Math.random() * 9 + 1)}.jpg`
-        }
-        className="object-fill w-96 h-48"
-        alt="image"
-      />
+      {property?.images[0]?.url ? (
+        <Image
+          radius="none"
+          src={
+            property?.images[0]?.url
+            // property.id === 1
+            //   ? property.images[0].url
+            //   : `/images/${Math.floor(Math.random() * 9 + 1)}.jpg`
+          }
+          className="object-fill w-96 h-48"
+          alt="image"
+        />
+      ) : (
+        <Image
+          radius="none"
+          src="/imageNotFound.png"
+          className="object-fill w-96 h-48"
+          alt="image"
+        />
+      )}
       <div className="flex flex-col mt-auto">
         <div className="p-4">
-          <p className="text-primary-600 text-xl font-bold">{property.name}</p>
-          <p className="text-slate-600">{property.location?.city}</p>
-          <p className="text-slate-600">{property.location?.state}</p>
+          <p className="text-primary-600 text-xl font-bold">{property?.name}</p>
+          <p className="text-slate-600">{property?.location?.city}</p>
+          <p className="text-slate-600">{property?.location?.state}</p>
         </div>
         <div className="bg-gradient-to-br from-slate-50 to-slate-400 p-4 flex justify-between">
           <p className="text-primary-600 text-xl font-bold">
-            {property.price.toLocaleString()} <span>F CFA</span>
+            {property?.price.toLocaleString()} <span>F CFA</span>
           </p>
           <Link
             className="hover:text-primary-500 transition-colors"
