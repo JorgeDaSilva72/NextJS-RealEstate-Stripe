0e0a038d6c677cb680ff63ddb428dc23bcd3b409
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-12-04 07:12:36 +0100

    Ajouter une gestion du chargement pour les annonces favorites.

--- src/app/user/favoriteProperties/page.tsx
+++ src/app/user/favoriteProperties/page.tsx
@@ -1,59 +1,3 @@
-// "use client";
-// import PropertyCard from "@/app/components/PropertyCard";
-// import { useFavorites } from "@/app/context/FavoriteContext";
-
-// interface Property {
-//   id: number;
-//   name: string;
-//   price: number;
-//   type?: {
-//     value: string;
-//   };
-//   status?: {
-//     value: string;
-//   };
-//   images: {
-//     url: string;
-//   }[];
-//   location?: { city: string; state: string } | null;
-//   feature?: {
-//     area: number;
-//     bedrooms: number;
-//     bathrooms: number;
-//     parkingSpots: number;
-//   } | null;
-// }
-
-// const FavoriteProperties = ({ properties }: { properties: Property[] }) => {
-//   const { favorites } = useFavorites();
-
-//   // Filtrer les propriétés favorites
-//   const favoriteProperties = (properties || []).filter((property) =>
-//     favorites.includes(property.id)
-//   );
-
-//   return (
-//     <div>
-//       <h1 className="text-2xl font-bold mb-4">Mes Favoris</h1>
-
-//       {favoriteProperties.length === 0 ? (
-//         <div className="text-gray-500">
-//           Vous n&apos;avez aucune propriété en favoris pour le moment.
-//         </div>
-//       ) : (
-//         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-//           {favoriteProperties.map((property) => (
-//             <PropertyCard key={property.id} property={property} />
-//           ))}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default FavoriteProperties;
-
-// end -----------------------------------------
 "use client";
 import React, { useEffect, useState } from "react";
 import prisma from "@/lib/prisma";
@@ -64,13 +8,14 @@ import { useFavorites } from "@/app/context/FavoriteContext";
 import PageTitle from "@/app/components/pageTitle";
 
 export default function FavoriteProperties() {
-  //   const { favorites } = useFavorites(); // Récupère les IDs favoris
-  const { favorites, clearFavorites } = useFavorites();
+  const { favorites, clearFavorites } = useFavorites(); // Récupère les IDs favoris
   const [properties, setProperties] = useState([]);
   console.log("Favorites :", favorites); // Vérifiez que cette valeur est correcte
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchFavorites = async () => {
+      setIsLoading(true); // Commence le chargement
       try {
         const response = await fetch("/api/favorite-properties", {
           method: "POST",
@@ -79,7 +24,6 @@ export default function FavoriteProperties() {
           },
           body: JSON.stringify({ ids: favorites }),
         });
-        console.log("Réponse brute :", response);
         if (!response.ok) {
           console.error("Erreur lors de l'appel API :", response.statusText);
         }
@@ -91,6 +35,8 @@ export default function FavoriteProperties() {
           "Erreur lors du chargement des propriétés favorites :",
           error
         );
+      } finally {
+        setIsLoading(false); // Termine le chargement
       }
     };
 
@@ -107,14 +53,16 @@ export default function FavoriteProperties() {
         href="/result"
       />
 
-      {favorites.length && properties.length > 0 ? (
+      {isLoading ? (
+        <div className="text-center py-10">Chargement des annonces...</div>
+      ) : favorites.length && properties.length > 0 ? (
         <PropertyContainer totalPages={1} currentPage={1}>
           {properties.map((property, index) => (
             <PropertyCard key={index} property={property} />
           ))}
         </PropertyContainer>
       ) : (
-        <NoPropertiesFound /> // Affiche un message si aucun favori
+        <NoPropertiesFound />
       )}
     </div>
   );
