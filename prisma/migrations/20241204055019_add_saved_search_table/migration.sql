9c5da78ea6ecba21c0221ae4812c96e836d8c818
Rivah Ravelomanantsoa <rivahravelomanantsoa@gmail.com>
2024-12-04 08:51:20 +0300

    prisma

---
+++ prisma/migrations/20241204055019_add_saved_search_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "SavedSearch" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "filters" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "SavedSearch_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
