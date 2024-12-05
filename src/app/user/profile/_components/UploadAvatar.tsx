894e89cb8b0de2f8d09ab8260430871b79cb3ce3
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-12-05 15:42:59 +0100

    fix: corriger le bug d'upload d'avatar et suppression de l'ancienne photo (par Cedrico)
    - Ajout de la logique pour supprimer l'ancienne photo d'avatar lors de la mise à jour.
    - Contribution apportée par Cedrico.

--- src/app/user/profile/_components/UploadAvatar.tsx
+++ src/app/user/profile/_components/UploadAvatar.tsx
@@ -77,11 +77,111 @@
 
 // Cedrico : convertir image en webp et limitez la taille du fichier à 2Mo
 
+// "use client";
+
+// import FileInput from "@/app/components/fileUpload";
+// import { updateUserAvatar } from "@/lib/actions/user";
+// import { uploadAvatar, uploadImagesToWebp } from "@/lib/upload";
+// import { PencilIcon } from "@heroicons/react/16/solid";
+// import {
+//   Button,
+//   Image,
+//   Modal,
+//   ModalBody,
+//   ModalContent,
+//   ModalFooter,
+//   ModalHeader,
+//   useDisclosure,
+// } from "@nextui-org/react";
+// import { useRouter } from "next/navigation";
+// import { useState } from "react";
+// import fileToBase64 from "@/lib/fileToBase64";
+// import { toast } from "react-toastify";
+// import { MAX_SIZE_BYTES } from "../../properties/add/_components/Picture";
+
+// const UploadAvatar = ({ userId }: { userId: string }) => {
+//   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+//   const [image, setImage] = useState<File>();
+//   const [isSubmitting, setIsSubmitting] = useState(false);
+//   const router = useRouter();
+
+//   const handleChangeAvatar = (file: File) => {
+//     if (!file.type.startsWith("image/")) {
+//       toast.error("Le fichier doit être un image");
+//       return;
+//     } else if (file.size >= MAX_SIZE_BYTES) {
+//       toast.error("Le fichier est plus grand que 2Mo");
+//     }
+//     setImage(file);
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={onOpen}>
+//         <PencilIcon className="w-6 text-slate-400 hover:text-primary transition-colors" />
+//       </button>
+//       <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+//         <ModalContent>
+//           {(onClose) => (
+//             <>
+//               <ModalHeader className="flex flex-col gap-1">
+//                 Choisissez votre image:
+//               </ModalHeader>
+//               <ModalBody>
+//                 <FileInput
+//                   onChange={(e: any) => {
+//                     handleChangeAvatar(e.target.files[0]);
+//                   }}
+//                 />
+//                 {image && <Image src={URL.createObjectURL(image)} alt="" />}
+//               </ModalBody>
+//               <ModalFooter>
+//                 <Button color="danger" variant="light" onPress={onClose}>
+//                   Effacer
+//                 </Button>
+//                 <Button
+//                   isLoading={isSubmitting}
+//                   color="primary"
+//                   onPress={async () => {
+//                     setIsSubmitting(true);
+//                     if (!image) {
+//                       onClose();
+//                       return;
+//                     }
+//                     const imgBase64 = await fileToBase64(image);
+//                     const avatarUrl = await uploadImagesToWebp(
+//                       imgBase64,
+//                       image.name,
+//                       "avatars"
+//                     );
+//                     const result = await updateUserAvatar(avatarUrl, userId);
+//                     router.refresh();
+//                     setIsSubmitting(false);
+//                     onClose();
+//                   }}
+//                 >
+//                   Changer votre image
+//                 </Button>
+//               </ModalFooter>
+//             </>
+//           )}
+//         </ModalContent>
+//       </Modal>
+//     </div>
+//   );
+// };
+
+// export default UploadAvatar;
+
+// end ---------------------------------------
+
+// Cedrico : fixer upload avatar et supprimer l'ancien photo 05/12/2024
+
 "use client";
 
 import FileInput from "@/app/components/fileUpload";
 import { updateUserAvatar } from "@/lib/actions/user";
-import { uploadAvatar, uploadImagesToWebp } from "@/lib/upload";
+import { removeImages, uploadAvatar, uploadImagesToWebp } from "@/lib/upload";
 import { PencilIcon } from "@heroicons/react/16/solid";
 import {
   Button,
@@ -99,18 +199,27 @@ import fileToBase64 from "@/lib/fileToBase64";
 import { toast } from "react-toastify";
 import { MAX_SIZE_BYTES } from "../../properties/add/_components/Picture";
 
-const UploadAvatar = ({ userId }: { userId: string }) => {
+const UploadAvatar = ({
+  userId,
+  userAvatar,
+}: {
+  userId: string;
+  userAvatar: string | null;
+}) => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
-  const [image, setImage] = useState<File>();
+  const [image, setImage] = useState<File | null>();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
 
   const handleChangeAvatar = (file: File) => {
     if (!file.type.startsWith("image/")) {
       toast.error("Le fichier doit être un image");
+      setImage(null);
       return;
     } else if (file.size >= MAX_SIZE_BYTES) {
       toast.error("Le fichier est plus grand que 2Mo");
+      setImage(null);
+      return;
     }
     setImage(file);
   };
@@ -136,7 +245,14 @@ const UploadAvatar = ({ userId }: { userId: string }) => {
                 {image && <Image src={URL.createObjectURL(image)} alt="" />}
               </ModalBody>
               <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
+                <Button
+                  color="danger"
+                  variant="light"
+                  onPress={() => {
+                    setImage(null);
+                    onClose();
+                  }}
+                >
                   Effacer
                 </Button>
                 <Button
@@ -145,6 +261,7 @@ const UploadAvatar = ({ userId }: { userId: string }) => {
                   onPress={async () => {
                     setIsSubmitting(true);
                     if (!image) {
+                      setIsSubmitting(false);
                       onClose();
                       return;
                     }
@@ -154,6 +271,10 @@ const UploadAvatar = ({ userId }: { userId: string }) => {
                       image.name,
                       "avatars"
                     );
+                    const userAvatarFileName = userAvatar?.split("/").at(-1);
+                    if (userAvatarFileName)
+                      await removeImages([userAvatarFileName], "avatars");
+
                     const result = await updateUserAvatar(avatarUrl, userId);
                     router.refresh();
                     setIsSubmitting(false);
