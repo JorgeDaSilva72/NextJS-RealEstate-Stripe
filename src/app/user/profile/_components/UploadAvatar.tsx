c46118e4cbadb66d01f7e58658f63d4ed8985e6c
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-12-05 14:41:47 +0100

    feat: intégrer le code de Cedrico pour convertir les images en WebP et limiter leur taille à 3 Mo
    - Conversion automatique des images uploadées avatar et annonces au format WebP pour optimiser les performances.
    - Ajout d'une validation pour limiter la taille des fichiers uploadés à 3 Mo.
    - Contribution réalisée par Cedrico.

--- src/app/user/profile/_components/UploadAvatar.tsx
+++ src/app/user/profile/_components/UploadAvatar.tsx
@@ -1,8 +1,87 @@
+// "use client";
+
+// import FileInput from "@/app/components/fileUpload";
+// import { updateUserAvatar } from "@/lib/actions/user";
+// import { uploadAvatar } from "@/lib/upload";
+// import { PencilIcon } from "@heroicons/react/16/solid";
+// import {
+//   Button,
+//   Image,
+//   Modal,
+//   ModalBody,
+//   ModalContent,
+//   ModalFooter,
+//   ModalHeader,
+//   useDisclosure,
+// } from "@nextui-org/react";
+// import { useRouter } from "next/navigation";
+// import { useState } from "react";
+
+// const UploadAvatar = ({ userId }: { userId: string }) => {
+//   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+//   const [image, setImage] = useState<File>();
+//   const [isSubmitting, setIsSubmitting] = useState(false);
+//   const router = useRouter();
+
+//   return (
+//     <div>
+//       <button onClick={onOpen}>
+//         <PencilIcon className="w-6 text-slate-400 hover:text-primary transition-colors" />
+//       </button>
+//       <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+//         <ModalContent>
+//           {(onClose) => (
+//             <>
+//               <ModalHeader className="flex flex-col gap-1">
+//                 Choisissez votre image:
+//               </ModalHeader>
+//               <ModalBody>
+//                 <FileInput
+//                   onChange={(e: any) => setImage((e as any).target.files[0])}
+//                 />
+//                 {image && <Image src={URL.createObjectURL(image)} alt="" />}
+//               </ModalBody>
+//               <ModalFooter>
+//                 <Button color="danger" variant="light" onPress={onClose}>
+//                   Effacer
+//                 </Button>
+//                 <Button
+//                   isLoading={isSubmitting}
+//                   color="primary"
+//                   onPress={async () => {
+//                     setIsSubmitting(true);
+//                     if (!image) {
+//                       onClose();
+//                       return;
+//                     }
+//                     const avatarUrl = await uploadAvatar(image);
+//                     const result = await updateUserAvatar(avatarUrl, userId);
+//                     router.refresh();
+//                     setIsSubmitting(false);
+//                     onClose();
+//                   }}
+//                 >
+//                   Changer votre image
+//                 </Button>
+//               </ModalFooter>
+//             </>
+//           )}
+//         </ModalContent>
+//       </Modal>
+//     </div>
+//   );
+// };
+
+// export default UploadAvatar;
+// end ---------------------------------------
+
+// Cedrico : convertir image en webp et limitez la taille du fichier à 2Mo
+
 "use client";
 
 import FileInput from "@/app/components/fileUpload";
 import { updateUserAvatar } from "@/lib/actions/user";
-import { uploadAvatar } from "@/lib/upload";
+import { uploadAvatar, uploadImagesToWebp } from "@/lib/upload";
 import { PencilIcon } from "@heroicons/react/16/solid";
 import {
   Button,
@@ -16,12 +95,25 @@ import {
 } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import fileToBase64 from "@/lib/fileToBase64";
+import { toast } from "react-toastify";
+import { MAX_SIZE_BYTES } from "../../properties/add/_components/Picture";
 
 const UploadAvatar = ({ userId }: { userId: string }) => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const [image, setImage] = useState<File>();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
+
+  const handleChangeAvatar = (file: File) => {
+    if (!file.type.startsWith("image/")) {
+      toast.error("Le fichier doit être un image");
+      return;
+    } else if (file.size >= MAX_SIZE_BYTES) {
+      toast.error("Le fichier est plus grand que 2Mo");
+    }
+    setImage(file);
+  };
 
   return (
     <div>
@@ -37,7 +129,9 @@ const UploadAvatar = ({ userId }: { userId: string }) => {
               </ModalHeader>
               <ModalBody>
                 <FileInput
-                  onChange={(e: any) => setImage((e as any).target.files[0])}
+                  onChange={(e: any) => {
+                    handleChangeAvatar(e.target.files[0]);
+                  }}
                 />
                 {image && <Image src={URL.createObjectURL(image)} alt="" />}
               </ModalBody>
@@ -54,7 +148,12 @@ const UploadAvatar = ({ userId }: { userId: string }) => {
                       onClose();
                       return;
                     }
-                    const avatarUrl = await uploadAvatar(image);
+                    const imgBase64 = await fileToBase64(image);
+                    const avatarUrl = await uploadImagesToWebp(
+                      imgBase64,
+                      image.name,
+                      "avatars"
+                    );
                     const result = await updateUserAvatar(avatarUrl, userId);
                     router.refresh();
                     setIsSubmitting(false);
--- src/app/user/properties/add/_components/Picture.tsx
+++ src/app/user/properties/add/_components/Picture.tsx
@@ -264,6 +264,296 @@
 
 // -------JhnRavelo fixer le bug de la suppression de l'image
 
+// import React, { useEffect, useState } from "react";
+// import { Button, Card } from "@nextui-org/react";
+// import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/16/solid";
+// import { PropertyImage, PropertyVideo } from "@prisma/client";
+// import Image from "next/image";
+// import PictureCard from "./PictureCard";
+// import { toast } from "react-toastify";
+// import ButtonClose from "@/app/components/ButtonClose";
+
+// interface Props {
+//   next: () => void;
+//   prev: () => void;
+//   className?: string;
+//   images: File[];
+//   setImages: (images: File[]) => void;
+//   videos?: string[];
+//   setVideos?: (videos: string[]) => void;
+//   savedImagesUrl?: PropertyImage[];
+//   setSavedImageUrl?: (propertyImages: PropertyImage[]) => void;
+//   savedVideosUrl?: PropertyVideo[];
+//   setSavedVideoUrl?: (propertyVideos: PropertyVideo[]) => void;
+//   maxImages: number;
+//   maxVideos?: number;
+//   isPremium: boolean;
+// }
+
+// const Picture = ({
+//   next,
+//   prev,
+//   className,
+//   images,
+//   setImages,
+//   savedImagesUrl = [],
+//   setSavedImageUrl,
+//   savedVideosUrl = [],
+//   setSavedVideoUrl,
+//   maxImages,
+//   videos = [],
+//   setVideos,
+//   maxVideos = 0,
+//   isPremium,
+// }: Props) => {
+//   const [error, setError] = useState<string | null>(null);
+//   const [videoUrl, setVideoUrl] = useState<string>("");
+
+//   // Utiliser useEffect pour afficher le toast seulement lorsque l'erreur change
+//   useEffect(() => {
+//     if (error) {
+//       toast.error(error);
+//     }
+//   }, [error]);
+
+//   // Gère l'ajout des images
+//   const handleSelectImages = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     const selectedFiles = Array.from(event.target.files as FileList);
+
+//     if (
+//       images.length + savedImagesUrl.length + selectedFiles.length >
+//       maxImages
+//     ) {
+//       setError(`Vous ne pouvez pas télécharger plus de ${maxImages} images.`);
+//       return;
+//     }
+
+//     const validFiles = selectedFiles.filter((file) =>
+//       file.type.startsWith("image/")
+//     );
+
+//     if (validFiles.length !== selectedFiles.length) {
+//       setError("Certains fichiers ne sont pas des images valides.");
+//       return;
+//     }
+
+//     setError(null);
+//     setImages([...validFiles, ...images]);
+//   };
+
+//   // Gère la suppression des images locales
+//   const removeImage = (index: number) => {
+//     setImages(images.filter((_, i) => i !== index));
+//   };
+
+//   // Gère la suppression des images sauvegardées
+//   const removeSavedImage = (id: number) => {
+//     setSavedImageUrl?.(savedImagesUrl.filter((img) => img.id !== id));
+//   };
+
+//   // Gère l'ajout de vidéos
+//   const handleAddVideo = () => {
+//     if (!isPremium) {
+//       setError(
+//         "Seuls les utilisateurs avec le PACK ELITE DIAMANT peuvent ajouter une vidéo."
+//       );
+//       return;
+//     }
+
+//     const youtubeRegex =
+//       /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/;
+
+//     if (!youtubeRegex.test(videoUrl)) {
+//       setError("Lien YouTube non valide.");
+//       return;
+//     }
+
+//     if (savedVideosUrl.length >= maxVideos) {
+//       setError(`Vous ne pouvez pas ajouter plus de ${maxVideos} vidéos.`);
+//       return;
+//     }
+
+//     setError(null);
+//     setVideos?.([...videos, videoUrl]);
+//     setVideoUrl("");
+//   };
+
+//   // Gère la suppression des vidéos
+//   const removeVideo = (index: number) => {
+//     setVideos?.(videos.filter((_, i) => i !== index));
+//   };
+
+//   const removeSavedVideo = (id: number) => {
+//     const videos = savedVideosUrl.filter((video) => video.id !== id);
+//     setSavedVideoUrl?.(videos);
+//   };
+
+//   return (
+//     <Card className={`p-4 ${className}`}>
+//       {/* Gestion des images */}
+//       <div>
+//         <input
+//           type="file"
+//           accept="image/*"
+//           multiple
+//           onChange={handleSelectImages}
+//           className="hidden"
+//           id="image-upload"
+//         />
+//         <label
+//           htmlFor="image-upload"
+//           className="cursor-pointer border border-dashed p-4 text-center rounded-lg hover:bg-gray-100"
+//         >
+//           Cliquez pour sélectionner des images ou déposez-les ici
+//         </label>
+//         {/* {error && <p className="text-red-500 mt-2">{error}</p>} */}
+//         {/* Affichage du Toast si une erreur existe */}
+//         {/* {error && toast.error(`${error}`)} */}
+//         <div className="flex flex-wrap gap-4 mt-4">
+//           {savedImagesUrl?.map((image, index) => (
+//             <PictureCard
+//               key={`saved-${image.id}`}
+//               src={image.url}
+//               index={index}
+//               onDelete={(idx) => removeSavedImage(savedImagesUrl[idx].id)} // Gérer la suppression avec l'index
+//             />
+//           ))}
+//           {images.map((file, index) => (
+//             <div key={`new-${index}`} className="relative">
+//               <Image
+//                 width={300}
+//                 height={200}
+//                 src={URL.createObjectURL(file)}
+//                 alt={file.name}
+//                 className="w-24 h-24 object-cover rounded-lg"
+//               />
+//               <ButtonClose
+//                 top="top-1"
+//                 right="right-1"
+//                 width="w-6"
+//                 height="h-6"
+//                 onClick={() => removeImage(index)}
+//               />
+//               {/* <button
+//                 onClick={() => removeImage(index)}
+//                 className="absolute z-50 top-1 right-1 w-6 h-6 flex items-center justify-center bg-red-500 text-white rounded-full hover:bg-red-600 transition-colors"
+//               >
+//                 ✕
+//               </button> */}
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+
+//       {/* Gestion des vidéos */}
+//       <div className="mt-6">
+//         <label className="block text-sm font-medium text-gray-700">
+//           Ajouter une vidéo YouTube
+//         </label>
+//         {/* {!isPremium ? ( */}
+//         {false ? (
+//           <p className="text-gray-500 mt-2">
+//             <strong>Note :</strong> Seuls les utilisateurs avec le PACK ELITE
+//             DIAMANT peuvent ajouter une vidéo.
+//           </p>
+//         ) : (
+//           <div className="flex gap-2 mt-2">
+//             <input
+//               type="text"
+//               value={videoUrl}
+//               onChange={(e) => setVideoUrl(e.target.value)}
+//               placeholder="Entrez un lien YouTube"
+//               className="flex-grow p-2 border rounded-lg"
+//             />
+//             <Button onClick={handleAddVideo} color="primary">
+//               Ajouter
+//             </Button>
+//           </div>
+//         )}
+
+//         <div className="mt-4 flex flex-wrap gap-4">
+//           {savedVideosUrl?.map((video, index) => (
+//             <div key={index} className="relative w-36 h-36">
+//               <iframe
+//                 src={`https://www.youtube.com/embed/${new URL(
+//                   video.url
+//                 ).searchParams.get("v")}`}
+//                 frameBorder="0"
+//                 allowFullScreen
+//                 className="w-full h-full object-contain rounded"
+//               />
+//               <ButtonClose
+//                 top="top-1"
+//                 right="right-1"
+//                 width="w-6"
+//                 height="h-6"
+//                 onClick={() => removeSavedVideo(savedVideosUrl[index].id)}
+//               />
+//               {/* <button
+//                 onClick={() => removeVideo(index)}
+//                 className="absolute top-1 right-1 bg-red-500 text-white rounded-full p-1"
+//               >
+//                 &times;
+//               </button> */}
+//             </div>
+//           ))}
+
+//           {videos.map((video, index) => (
+//             <div key={index} className="relative w-36 h-36">
+//               <iframe
+//                 src={`https://www.youtube.com/embed/${new URL(
+//                   video
+//                 ).searchParams.get("v")}`}
+//                 frameBorder="0"
+//                 allowFullScreen
+//                 className="w-full h-full object-contain rounded"
+//               />
+//               <ButtonClose
+//                 top="top-1"
+//                 right="right-1"
+//                 width="w-6"
+//                 height="h-6"
+//                 onClick={() => removeVideo(index)}
+//               />
+//               {/* <button
+//                 onClick={() => removeVideo(index)}
+//                 className="absolute top-1 right-1 bg-red-500 text-white rounded-full p-1"
+//               >
+//                 &times;
+//               </button> */}
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+
+//       {/* Boutons navigation */}
+//       <div className="flex justify-center gap-4 mt-6">
+//         <Button
+//           onClick={prev}
+//           startContent={<ChevronLeftIcon className="w-6" />}
+//           color="primary"
+//           className="w-full md:w-36"
+//         >
+//           Précédent
+//         </Button>
+//         <Button
+//           onClick={next}
+//           endContent={<ChevronRightIcon className="w-6" />}
+//           color="primary"
+//           className="w-full md:w-36"
+//         >
+//           Suivant
+//         </Button>
+//       </div>
+//     </Card>
+//   );
+// };
+
+// export default Picture;
+
+// end ---------------------------------------
+
+// Cedrico : convertir image en webp et limitez la taille du fichier à 2Mo
 import React, { useEffect, useState } from "react";
 import { Button, Card } from "@nextui-org/react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/16/solid";
@@ -289,6 +579,9 @@ interface Props {
   maxVideos?: number;
   isPremium: boolean;
 }
+
+const MAX_SIZE_MB = 2; // Limite en Mo
+export const MAX_SIZE_BYTES = MAX_SIZE_MB * 1024 * 1024; // Conversion en octets
 
 const Picture = ({
   next,
@@ -337,8 +630,17 @@ const Picture = ({
       return;
     }
 
+    const validFileSizes = selectedFiles.filter(
+      (file) => file.size <= MAX_SIZE_BYTES
+    );
+
+    if (selectedFiles.length !== validFileSizes.length) {
+      setError("Certains fichiers sont plus grand que 2Mo.");
+      return;
+    }
+
     setError(null);
-    setImages([...validFiles, ...images]);
+    setImages([...validFileSizes, ...images]);
   };
 
   // Gère la suppression des images locales
