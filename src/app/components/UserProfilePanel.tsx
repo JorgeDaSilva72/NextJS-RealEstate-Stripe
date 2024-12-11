c3d7a027a60a0eeba3677036ff14888a11c394d7
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-12-11 10:56:39 +0100

    Refactor :UserProfilePanel.
    - Ajout de commentaires explicatifs pour améliorer la lisibilité.
    - Refactorisation de la logique de gestion des classes dynamiques pour les options du menu (avec getOptionClass).
    - Simplification et amélioration des hooks useEffect pour la gestion de la largeur de fenêtre et la vérification de l'avatar utilisateur.
    - Consolidation du calcul de position du menu via getMenuPositionClass.
    - Retrait de code inutilisé/commenté pour réduire la complexité.
    - Utilisation de bonnes pratiques avec TypeScript et React.

--- src/app/components/UserProfilePanel.tsx
+++ src/app/components/UserProfilePanel.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 import { checkFileExists } from "@/lib/upload";
 import Image from "next/image";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { userOptions } from "../data/userOptions";
 
 interface Props {
@@ -20,20 +20,24 @@ const UserProfilePanel = ({ user }: Props) => {
   const optionsRef = useRef<HTMLDivElement>(null);
   const [isOpen, setIsOpen] = useState(false);
   const [windowWidth, setWindowWidth] = useState(0);
+  const router = useRouter();
 
-  // Ce useEffect est déclenché une fois que le composant est monté côté client
   useEffect(() => {
-    // Vérification de l'avatar uniquement après le montage côté client
-    (async () => {
-      if (user.avatarUrl) {
-        const avatarFileName = user.avatarUrl.split("/").at(-1);
+    // Vérification de l'avatar utilisateur uniquement après le montage côté client
+    if (user.avatarUrl) {
+      (async () => {
+        const avatarFileName = user.avatarUrl?.split("/").at(-1);
         if (avatarFileName) {
           const isAvatar = await checkFileExists("avatars", avatarFileName);
           setIsAvatar(isAvatar);
         }
-      }
-    })();
+      })();
+    }
+  }, [user.avatarUrl]);
 
+  // Gestion de la largeur de fenêtre
+  // Ce useEffect est déclenché une fois que le composant est monté côté client
+  useEffect(() => {
     // Mise à jour de la largeur de la fenêtre côté client
     const updateWindowWidth = () => setWindowWidth(window.innerWidth);
 
@@ -47,15 +51,41 @@ const UserProfilePanel = ({ user }: Props) => {
     return () => {
       window.removeEventListener("resize", updateWindowWidth);
     };
-  }, [user]); // Effectivement, ici on s'assure que ça s'exécute au montage initial et au changement de `user`
+  }, []);
 
-  const optionClassName =
-    "option hover:scale-95 hover:text-white shadow-md border border-[#ccc] cursor-pointer rounded-md text-[#333] bg-[#f9f9f9] p-3 flex items-center gap-3 transition-all duration-300 ease-in-out transform ";
+  // const optionClassName =
+  //   "option hover:scale-95 hover:text-white shadow-md border border-[#ccc] cursor-pointer rounded-md text-[#333] bg-[#f9f9f9] p-3 flex items-center gap-3 transition-all duration-300 ease-in-out transform ";
 
   const handleLinkClick = () => {
     // Ferme le menu lorsqu'un lien est cliqué
     setIsOpen(false);
   };
+
+  // Gestion de la largeur de fenêtre
+  const getMenuPositionClass = () => {
+    if (windowWidth < 600 && pathname !== "/") return "top-[9vh] right-[5vw]";
+    return "top-[6.5vh] right-[2.5vw]";
+  };
+
+  const isMenuVisible = isOpen
+    ? "opacity-100 pointer-events-auto scale-100"
+    : "opacity-0 pointer-events-none scale-0";
+
+  const getOptionClass = (url: string) =>
+    `option hover:scale-95 hover:text-white shadow-md border border-[#ccc] cursor-pointer rounded-md text-[#333] bg-[#f9f9f9] p-3 flex items-center gap-3 transition-all duration-300 ease-in-out transform ${
+      url === "/logout"
+        ? "hover:bg-red-600"
+        : url === "/user/favoriteProperties"
+        ? "hover:bg-[#e685c2d4]"
+        : url === "/user/properties"
+        ? "hover:bg-blue-500"
+        : "hover:bg-gray-600"
+    }`;
+
+  const getRedirectURL = () =>
+    process.env.NODE_ENV === "production"
+      ? "https://afriqueavenirimmobilier.com/"
+      : "http://localhost:3000/";
 
   return (
     <>
@@ -66,14 +96,16 @@ const UserProfilePanel = ({ user }: Props) => {
         }`}
       >
         <Image
-          src={isAvatar && user.avatarUrl ? user.avatarUrl : "/user.png"}
+          // src={isAvatar && user.avatarUrl ? user.avatarUrl : "/user.png"}
+          src={user.avatarUrl || "/user.png"}
+          onError={(e) => (e.currentTarget.src = "/user.png")}
           alt="Image profil"
           width={48}
           height={48}
           className="w-full h-full object-cover rounded-full"
         />
       </div>
-      <div
+      {/* <div
         className={`w-auto h-auto flex flex-col gap-2 transition-all duration-300 ease-in-out absolute md:top-[3vh] md:right-[4.7vw] top-[6.5vh] right-[2.5vw] ${
           windowWidth < 600 && pathname !== "/" ? "top-[9vh]" : "top-[6.5vh]"
         } ${
@@ -86,12 +118,23 @@ const UserProfilePanel = ({ user }: Props) => {
         //   backdropFilter: "blur(20px)", // Applique le flou de l'arrière-plan
         //   backgroundColor: "rgba(255, 255, 255, 0.6)", // Arrière-plan légèrement transparent
         // }}
+      > */}
+      <div
+        className={` w-auto h-auto flex flex-col gap-2 transition-all duration-300 ease-in-out absolute md:top-[3vh] md:right-[4.7vw] ${getMenuPositionClass()} ${isMenuVisible} z-[1000] bg-white/90 rounded-lg`}
+        ref={optionsRef}
       >
         {userOptions.map((option, index) => (
           <div key={index}>
             {option.url === "/logout" ? (
               <LogoutLink
-                className={`${optionClassName} hover:bg-red-600`}
+                // className={`${optionClassName} hover:bg-red-600`}
+                className={getOptionClass(option.url)}
+                postLogoutRedirectURL={getRedirectURL()}
+                onClick={() => {
+                  setTimeout(() => {
+                    router.push(getRedirectURL());
+                  }, 1000);
+                }}
                 // onClick={handleLinkClick} // Ferme le menu au clic sur Déconnexion
               >
                 {option.svg}
@@ -100,13 +143,14 @@ const UserProfilePanel = ({ user }: Props) => {
             ) : (
               <Link
                 href={option.url}
-                className={`${optionClassName} ${
-                  option.url == "/user/favoriteProperties"
-                    ? "hover:bg-[#e685c2d4]"
-                    : option.url == "/user/properties"
-                    ? "hover:bg-blue-500"
-                    : "hover:bg-gray-600"
-                }`}
+                className={getOptionClass(option.url)}
+                // className={`${optionClassName} ${
+                //   option.url == "/user/favoriteProperties"
+                //     ? "hover:bg-[#e685c2d4]"
+                //     : option.url == "/user/properties"
+                //     ? "hover:bg-blue-500"
+                //     : "hover:bg-gray-600"
+                // }`}
                 onClick={handleLinkClick} // Ferme le menu au clic sur un lien
               >
                 {option.svg}
