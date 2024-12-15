ac39430944bb6e94bc2cc71f535d443b1c572182
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-12-15 11:11:15 +0100

    feat(api/auth/success/route.ts): set default avatar for new users thanks to api dicebar.

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     serverActions: {
       bodySizeLimit: "3mb",
     },
   },
+  // images: {
+  //   domains: ["sejwzcphuueybcsdraw.supabase.co"], // Ajoutez ici le domaine exact
+  // },
   images: {
-    domains: ["sejwzcphuueybcsdraw.supabase.co"], // Ajoutez ici le domaine exact
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sejwzcphuueybcsdraw.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "api.dicebear.com",
+        pathname: "/6.x/identicon/svg",
+      },
+    ],
+    dangerouslyAllowSVG: true,
   },
 };
 
--- src/app/api/auth/success/route.ts
+++ src/app/api/auth/success/route.ts
@@ -3,6 +3,11 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
+//Génération d'avatars dynamiques
+function generateAvatarUrl(userId: string): string {
+  return `https://api.dicebear.com/6.x/identicon/svg?seed=${userId}`;
+}
+
 export async function GET(req: NextRequest) {
   try {
     // Obtenez la session et l'utilisateur
@@ -24,23 +29,30 @@ export async function GET(req: NextRequest) {
     // Définissez l'URL de redirection en fonction de l'environnement
     const baseUrl =
       process.env.NODE_ENV === "production"
-        ? // ? "https://afrique-avenir-immo.vercel.app/"
-          "https://afriqueavenirimmobilier.com/"
+        ? "https://afriqueavenirimmobilier.com/"
         : "http://localhost:3000/";
+
+    // A essqyer
+    //const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000/";
 
     // Si l'utilisateur n'existe pas, créez-le
     if (!dbUser) {
-      await prisma.user.create({
+      const newUser = await prisma.user.create({
         data: {
           id: user.id,
           firstName: user.given_name ?? "",
           lastName: user.family_name ?? "",
           email: user.email ?? "",
+          avatarUrl: generateAvatarUrl(user.id),
         },
       });
-
+      // return NextResponse.json({
+      //   message: "User created successfully",
+      //   user: newUser,
+      // });
+      console.log("User created:", newUser);
       // Redirigez vers la page d'accueil après la création de l'utilisateur
-      return NextResponse.redirect(baseUrl);
+      // return NextResponse.redirect(baseUrl);
     }
 
     // Si l'utilisateur existe déjà, redirigez également
