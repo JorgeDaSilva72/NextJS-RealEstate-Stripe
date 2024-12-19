ab86e1d6acc466f1c728f3dc91292e782d18c0a7
JhnRavelo <johnravelo135@gmail.com>
2024-12-19 11:42:13 +0300

    condition pour le header de la page properties

--- src/app/user/properties/layout.tsx
+++ src/app/user/properties/layout.tsx
@@ -1,5 +1,4 @@
-import { Button, modal } from "@nextui-org/react";
-import Link from "next/link";
+import HeaderPropertiesWrapper from "@/app/components/HeaderPropertiesWrapper";
 import React, { ReactNode } from "react";
 
 interface Props {
@@ -9,12 +8,7 @@ interface Props {
 const PropertiesLayout = ({ children, modalDelete }: Props) => {
   return (
     <div>
-      <div className="bg-primary-400 flex justify-between items-center p-2">
-        <h2 className="text-white text-xl font-semibold px-2">Mes annonces</h2>
-        <Button color="secondary">
-          <Link href="/user/properties/add">Créer une annonce</Link>
-        </Button>
-      </div>
+      <HeaderPropertiesWrapper />
       {children}
       <div>{modalDelete}</div>
     </div>
