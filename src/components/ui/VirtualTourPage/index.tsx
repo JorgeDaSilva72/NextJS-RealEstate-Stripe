c043cd8dbeee3533a7ff8d5e8d9c81aa530d4708
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2025-03-31 08:01:43 +0200

    feat(hero): translate in all languages button to watch video.

--- src/components/ui/VirtualTourPage/index.tsx
+++ src/components/ui/VirtualTourPage/index.tsx
@@ -477,7 +477,7 @@ const HeroSection = () => {
                   d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
                 />
               </svg>
-              Voir la vidéo
+              {t("hero.buttonSeeTheVideo")}
             </motion.button>
           </div>
         </div>
