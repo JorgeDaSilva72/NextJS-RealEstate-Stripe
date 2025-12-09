5adbaa5581b4a605132fcc394ca37b7c0080e375
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2025-12-09 15:02:12 +0100

    I commented out some code that I can't use now.

--- src/app/[locale]/user/properties/[id]/appointment/_components/AppointmentContainer.tsx
+++ src/app/[locale]/user/properties/[id]/appointment/_components/AppointmentContainer.tsx
@@ -1,160 +1,162 @@
-"use client";
-import { AppointmentEvent } from "@/app/[locale]/property/[id]/appointment/page";
-import {
-  changeAppointmentState,
-  getAppointmentsByProperty,
-} from "@/lib/actions/appointment";
-import { Property, PropertyLocation, User } from "@prisma/client";
-import React, { useEffect, useState } from "react";
-import { toast } from "react-toastify";
-import { usePathname } from "next/navigation";
-import emailjs from "@emailjs/browser";
-import { isUserDiamant } from "@/lib/actions/user";
-import { useRouter } from "next/router";
-import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
+// 09-12-2025 commenté provisoirement car  le code  doit être retouché pour qu il fonctionne avec la nouvelle structure de la base de données mais comme cette page n a pas été bien défini on ne va pas plus loin
 
-interface AppointmentContainerProps {
-  id: number;
-}
+// "use client";
+// import { AppointmentEvent } from "@/app/[locale]/property/[id]/appointment/page";
+// import {
+//   changeAppointmentState,
+//   getAppointmentsByProperty,
+// } from "@/lib/actions/appointment";
+// import { Property, PropertyLocation, User } from "@prisma/client";
+// import React, { useEffect, useState } from "react";
+// import { toast } from "react-toastify";
+// import { usePathname } from "next/navigation";
+// import emailjs from "@emailjs/browser";
+// import { isUserDiamant } from "@/lib/actions/user";
+// import { useRouter } from "next/router";
+// import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
 
-export type AppointmentWithUserAndProperty = AppointmentEvent & {
-  user: User;
-  property: Property & {
-    user: User;
-    location: PropertyLocation | null;
-  };
-};
+// interface AppointmentContainerProps {
+//   id: number;
+// }
 
-const AppointmentContainer = ({ id }: AppointmentContainerProps) => {
-  const [appointments, setAppointments] = useState<
-    AppointmentWithUserAndProperty[]
-  >([]);
-  const pathname = usePathname();
-  const classNameInfo = "flex gap-7";
-  const router = useRouter();
-  const { user } = useKindeAuth();
+// export type AppointmentWithUserAndProperty = AppointmentEvent & {
+//   user: User;
+//   property: Property & {
+//     user: User;
+//     location: PropertyLocation | null;
+//   };
+// };
 
-  useEffect(() => {
-    (async () => {
-      if (user?.id) {
-        const foundUser = await isUserDiamant(undefined, user.id);
-        if (!foundUser) return router.back();
-      }
-      const result = await getAppointmentsByProperty(id);
-      if (result.success) {
-        setAppointments(result.data);
-      } else toast.error(result.message);
-    })();
-  }, [id, user?.id, router]);
+// const AppointmentContainer = ({ id }: AppointmentContainerProps) => {
+//   const [appointments, setAppointments] = useState<
+//     AppointmentWithUserAndProperty[]
+//   >([]);
+//   const pathname = usePathname();
+//   const classNameInfo = "flex gap-7";
+//   const router = useRouter();
+//   const { user } = useKindeAuth();
 
-  const handleClick = async (
-    appointment: AppointmentWithUserAndProperty,
-    state: string
-  ) => {
-    try {
-      if (!appointment.id) return;
-      console.log(process.env.NEXT_PUBLIC_SERVICE_ID, state);
-      await emailjs.send(
-        process.env.NEXT_PUBLIC_SERVICE_ID!, // Remplacez par votre service ID
-        process.env.NEXT_PUBLIC_TEMPLATE_ID!, // Remplacez par votre template ID
-        {
-          to_name: appointment.user.lastName,
-          to_email: appointment.user.email,
-          from_name:
-            appointment.property.user.firstName +
-            " " +
-            appointment.property.user.lastName,
-          appointment_date: new Date(appointment.start)
-            .toISOString()
-            .slice(0, 10),
-          appointment_time: new Date(appointment.start)
-            .toTimeString()
-            .slice(0, 6),
-          appointment_title: appointment.property.name,
-          appointment_country: appointment.property.location?.state,
-          appointment_location:
-            appointment.property.location?.city +
-            " / " +
-            appointment.property.location?.region +
-            " / " +
-            appointment.property.location?.streetAddress,
-          status_message: state == "accepted" ? "Accepté ✅" : "Refusé ❌",
-          status_color: state == "accepted" ? "green" : "red",
-        },
-        process.env.NEXT_PUBLIC_PUBLIC_KEY_EMAIL! // Remplacez par votre clé publique
-      );
-      toast.success("Email envoyé avec succès !");
-      const result = await changeAppointmentState(appointment.id, state, id);
-      if (result.success) {
-        setAppointments(result.data);
-      } else toast.error(result.message);
-    } catch (error) {
-      toast.error("Erreur lors de l'envoi de l'email :" + error);
-    }
-  };
+//   useEffect(() => {
+//     (async () => {
+//       if (user?.id) {
+//         const foundUser = await isUserDiamant(undefined, user.id);
+//         if (!foundUser) return router.back();
+//       }
+//       const result = await getAppointmentsByProperty(id);
+//       if (result.success) {
+//         setAppointments(result.data);
+//       } else toast.error(result.message);
+//     })();
+//   }, [id, user?.id, router]);
 
-  return (
-    <div className="flex flex-col gap-4">
-      <div className="scrollbar-default flex md:flex-row flex-col flex-wrap gap-6 justify-between px-5 pt-5 w-full max-h-[70vh] overflow-y-auto">
-        {appointments.length > 0 &&
-          appointments
-            .filter((appointment) => {
-              if (pathname.includes("accept")) {
-                return appointment.state == "accepted";
-              } else return appointment.state == "pending";
-            })
-            .map((appointment, index) => (
-              <div
-                className="xl:w-[23%] h-[fit-content] md:w-[45%] w-full flex flex-col gap-2 border-2 border-blue-500 shadow-md rounded-md text-blue-900 p-2"
-                key={index}
-              >
-                <div className={classNameInfo}>
-                  <span>Titre :</span>
-                  <span>{appointment.property.name}</span>
-                </div>
-                <div className={classNameInfo}>
-                  <span>Date de la visite :</span>
-                  <span>
-                    {new Date(appointment.start).toISOString().slice(0, 10)}
-                  </span>
-                </div>
-                <div className={classNameInfo}>
-                  <span>De :</span>
-                  <span>
-                    {new Date(appointment.start).toTimeString().slice(0, 6)}
-                  </span>
-                </div>
-                <div className={classNameInfo}>
-                  <span>À&nbsp;&nbsp;&nbsp;:</span>
-                  <span>
-                    {new Date(appointment.end).toTimeString().slice(0, 6)}
-                  </span>
-                </div>
-                <div className={classNameInfo}>
-                  <span>Description :</span>
-                  <span>{appointment.title.replace("Description :", "")}</span>
-                </div>
-                {appointment.state == "pending" && (
-                  <div className="flex flex-col md:flex-row justify-between px-4 bg-white/10 backdrop-blur-sm">
-                    <button
-                      onClick={() => handleClick(appointment, "accepted")}
-                      className="px-4 py-2 bg-indigo-600 text-white rounded shadow-lg hover:bg-indigo-700 mb-4 md:mb-0 text-center"
-                    >
-                      Accepter
-                    </button>
-                    <button
-                      className="px-4 py-2 bg-red-600 text-white rounded shadow-lg hover:bg-red-700  text-center"
-                      onClick={() => handleClick(appointment, "refused")}
-                    >
-                      Refuser
-                    </button>
-                  </div>
-                )}
-              </div>
-            ))}
-      </div>
-    </div>
-  );
-};
+//   const handleClick = async (
+//     appointment: AppointmentWithUserAndProperty,
+//     state: string
+//   ) => {
+//     try {
+//       if (!appointment.id) return;
+//       console.log(process.env.NEXT_PUBLIC_SERVICE_ID, state);
+//       await emailjs.send(
+//         process.env.NEXT_PUBLIC_SERVICE_ID!, // Remplacez par votre service ID
+//         process.env.NEXT_PUBLIC_TEMPLATE_ID!, // Remplacez par votre template ID
+//         {
+//           to_name: appointment.user.lastName,
+//           to_email: appointment.user.email,
+//           from_name:
+//             appointment.property.user.firstName +
+//             " " +
+//             appointment.property.user.lastName,
+//           appointment_date: new Date(appointment.start)
+//             .toISOString()
+//             .slice(0, 10),
+//           appointment_time: new Date(appointment.start)
+//             .toTimeString()
+//             .slice(0, 6),
+//           appointment_title: appointment.property.name,
+//           appointment_country: appointment.property.location?.state,
+//           appointment_location:
+//             appointment.property.location?.city +
+//             " / " +
+//             appointment.property.location?.region +
+//             " / " +
+//             appointment.property.location?.streetAddress,
+//           status_message: state == "accepted" ? "Accepté ✅" : "Refusé ❌",
+//           status_color: state == "accepted" ? "green" : "red",
+//         },
+//         process.env.NEXT_PUBLIC_PUBLIC_KEY_EMAIL! // Remplacez par votre clé publique
+//       );
+//       toast.success("Email envoyé avec succès !");
+//       const result = await changeAppointmentState(appointment.id, state, id);
+//       if (result.success) {
+//         setAppointments(result.data);
+//       } else toast.error(result.message);
+//     } catch (error) {
+//       toast.error("Erreur lors de l'envoi de l'email :" + error);
+//     }
+//   };
 
-export default AppointmentContainer;
+//   return (
+//     <div className="flex flex-col gap-4">
+//       <div className="scrollbar-default flex md:flex-row flex-col flex-wrap gap-6 justify-between px-5 pt-5 w-full max-h-[70vh] overflow-y-auto">
+//         {appointments.length > 0 &&
+//           appointments
+//             .filter((appointment) => {
+//               if (pathname.includes("accept")) {
+//                 return appointment.state == "accepted";
+//               } else return appointment.state == "pending";
+//             })
+//             .map((appointment, index) => (
+//               <div
+//                 className="xl:w-[23%] h-[fit-content] md:w-[45%] w-full flex flex-col gap-2 border-2 border-blue-500 shadow-md rounded-md text-blue-900 p-2"
+//                 key={index}
+//               >
+//                 <div className={classNameInfo}>
+//                   <span>Titre :</span>
+//                   <span>{appointment.property.name}</span>
+//                 </div>
+//                 <div className={classNameInfo}>
+//                   <span>Date de la visite :</span>
+//                   <span>
+//                     {new Date(appointment.start).toISOString().slice(0, 10)}
+//                   </span>
+//                 </div>
+//                 <div className={classNameInfo}>
+//                   <span>De :</span>
+//                   <span>
+//                     {new Date(appointment.start).toTimeString().slice(0, 6)}
+//                   </span>
+//                 </div>
+//                 <div className={classNameInfo}>
+//                   <span>À&nbsp;&nbsp;&nbsp;:</span>
+//                   <span>
+//                     {new Date(appointment.end).toTimeString().slice(0, 6)}
+//                   </span>
+//                 </div>
+//                 <div className={classNameInfo}>
+//                   <span>Description :</span>
+//                   <span>{appointment.title.replace("Description :", "")}</span>
+//                 </div>
+//                 {appointment.state == "pending" && (
+//                   <div className="flex flex-col md:flex-row justify-between px-4 bg-white/10 backdrop-blur-sm">
+//                     <button
+//                       onClick={() => handleClick(appointment, "accepted")}
+//                       className="px-4 py-2 bg-indigo-600 text-white rounded shadow-lg hover:bg-indigo-700 mb-4 md:mb-0 text-center"
+//                     >
+//                       Accepter
+//                     </button>
+//                     <button
+//                       className="px-4 py-2 bg-red-600 text-white rounded shadow-lg hover:bg-red-700  text-center"
+//                       onClick={() => handleClick(appointment, "refused")}
+//                     >
+//                       Refuser
+//                     </button>
+//                   </div>
+//                 )}
+//               </div>
+//             ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AppointmentContainer;
--- src/app/[locale]/user/properties/[id]/appointment/layout.tsx
+++ src/app/[locale]/user/properties/[id]/appointment/layout.tsx
@@ -1,37 +1,39 @@
-import React, { ReactNode } from "react";
-import Options, { TypeOptions } from "./_components/Options";
-// import PageTitle from "@/app/components/pageTitle";
-import AppointmentContainer from "./_components/AppointmentContainer";
-import PageTitle from "@/app/[locale]/components/pageTitle";
+// 09-12-2025 commenté provisoirement car  le code  doit être retouché pour qu il fonctionne avec la nouvelle structure de la base de données mais comme cette page n a pas été bien défini on ne va pas plus loin
 
-interface Layout {
-  children: ReactNode;
-  params: { id: string };
-}
+// import React, { ReactNode } from "react";
+// import Options, { TypeOptions } from "./_components/Options";
+// // import PageTitle from "@/app/components/pageTitle";
+// import AppointmentContainer from "./_components/AppointmentContainer";
+// import PageTitle from "@/app/[locale]/components/pageTitle";
 
-const layout = ({ children, params }: Layout) => {
-  const options: TypeOptions = [
-    {
-      title: "Rendez-vous acceptée",
-      url: "/user/properties/" + params.id + "/appointment/accept",
-    },
-    {
-      title: "Nouveau Rendez-vous",
-      url: "/user/properties/" + params.id + "/appointment",
-    },
-  ];
-  return (
-    <>
-      <PageTitle
-        title="Mes Rendez-vous"
-        linkCaption="Mes annonces"
-        href="/user/properties/"
-      />
-      <Options options={options} />
-      <AppointmentContainer id={+params.id} />
-      {children}
-    </>
-  );
-};
+// interface Layout {
+//   children: ReactNode;
+//   params: { id: string };
+// }
 
-export default layout;
+// const layout = ({ children, params }: Layout) => {
+//   const options: TypeOptions = [
+//     {
+//       title: "Rendez-vous acceptée",
+//       url: "/user/properties/" + params.id + "/appointment/accept",
+//     },
+//     {
+//       title: "Nouveau Rendez-vous",
+//       url: "/user/properties/" + params.id + "/appointment",
+//     },
+//   ];
+//   return (
+//     <>
+//       <PageTitle
+//         title="Mes Rendez-vous"
+//         linkCaption="Mes annonces"
+//         href="/user/properties/"
+//       />
+//       <Options options={options} />
+//       <AppointmentContainer id={+params.id} />
+//       {children}
+//     </>
+//   );
+// };
+
+// export default layout;
